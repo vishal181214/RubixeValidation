cc2116fc1d8c0e74c789dd3cb54b22aa03ecf41f
vishal181214 <vishal32gaykwad@gmail.com>
2023-01-24 22:05:16 +0530

    validation done

---
+++ cmpassignment/src/data.jsx
@@ -0,0 +1,94 @@
+Mukundan Unni Associates
+2 hr 6 min, Crime, Comedy
+Malayalam, Hindi, Tamil, Telugu, 2022
+Fired from a law firm unethical conduct, Adv Mukundan Unni leaves ...
+
+Taaza Khabar
+Fantasy, Thriller, Hotstar Specials
+A public toilet caretaker's poverty-stricken life takes a drastic U-turn...
+
+Aar Ya Paar 
+Action, Crime, Thriller, Drama, Hotstar
+When the modern world's greed destroys his idyllic tribal existence....
+
+Jaya Jaya Jaya Jaya Hey
+2 hr 23 min
+Malayalam, Hindi, Tamil, Telugu, 2022
+Being brought up in a conservative family, jaya feels even more trapped...
+
+Govinda Naam Mera
+2 hr 11 min, Comedy, Drama, Hindi, 2022
+Wedged between a court case, a bad
+marriage and his girlfriend, Govinda....
+
+Jagamemaya: Instant Karma
+1 hr 51 min Mystery, Drama, Telugu,
+Malayalam, Hindi, Tamil, Kannada, 2022
+A conman tricks a grieving widow into
+marriage to make instant cash....
+
+Fall
+Mystry, Drama, Hotstar Specials
+An accident followed by memory loss
+has Divya trying to piece her life....
+
+Monstar
+2 hr 13 min, Thriller, Crime,
+Malayalam, Hindi, Tamil, Telugu, 2022
+Trying to cope with the hardships,
+Bhamini and Anil's life turns...
+
+Freddy
+2 hr 3 min, Drama, Thriller,Hindi 2022
+Introverted and awkward Dr Freddy
+Ginwala desperately searches for a....
+
+Willow
+Fantasy,Adventure,Disney
+Willow leads a group of misfit heroes
+on a dangerous rescue mission....
+
+Sita Ramam
+2 hr 33 min, Romancem Drama, Hindi,2022
+On a letter-delivering task, Afreen
+learns od a 20-year-old romance set....
+
+Rorschach
+2 hr 29 min, Thriller,
+Malayalam, Hindi, Tamil, Telugu,2022
+Luke Anthony comes to a village
+searching for his missing wife. His...
+
+Brahmastra Part One: Shiva
+2 hr 48 min,Fantasy,Adventure,
+Hindi,Malayalam,Tamil,Kannada,
+Telugu,2022
+A journey of fantasy,action and
+romance comes to life as Shiva enter...
+
+Jhansi
+Action,Drama,Hotstar Specials
+Jhansi's quest to uncover her past
+brings her onto the path of people wh...
+
+Sketch
+Action,Mystery,Select Picks
+A man who lost his love, and a woman
+who draws the future work hand-in-...
+
+Big Mouth
+Action, Drama, Select Picks
+A lawyer, caught up in murder case,
+becomes a notorious and genius con...
+
+Butterfly
+2 hr 16 min, Thriller, Telugu,
+Malayalam, Tamil, Kannada, 2022
+While trying to rescue her sister's kids
+from kidnappers, Geetha excavates...
+
+Prince
+2 hr 8min, Comedy, Romance, Tamil,
+Malayalam, Kannada, Telugu, 2022
+A comedy of errors ensues when Anbu
+falls for his British colleague Jessica....
